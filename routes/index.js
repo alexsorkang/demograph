13c546b1603b950a3e05e6d2529f049f131bd6b1
alex sor kang <asork42@berkeley.edu>
2017-03-08 14:12:25 -0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress || 
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
+  console.log(ip)
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;

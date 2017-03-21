eaae5454db7ad94cbf8cb3f171678fdd89942acb
alex sor kang <asork42@berkeley.edu>
2017-03-21 11:22:46 -0700

    displays correct data

--- routes/index.js
+++ routes/index.js
@@ -9,66 +9,22 @@ router.get('/', function(req, res, next) {
      req.connection.remoteAddress || 
      req.socket.remoteAddress ||
      req.connection.socket.remoteAddress;
-  // { ip: '172.88.99.210',
-  // country_code: 'US',
-  // country_name: 'United States',
-  // region_code: 'CA',
-  // region_name: 'California',
-  // city: 'Los Angeles',
-  // zip_code: '90004',
-  // time_zone: 'America/Los_Angeles',
-  // latitude: 34.0762,
-  // longitude: -118.3104,
-  // metro_code: 803 }
-
-  // { ip: '118.37.125.159',
-  // country_code: 'KR',
-  // country_name: 'Republic of Korea',
-  // region_code: '11',
-  // region_name: 'Seoul',
-  // city: '',
-  // zip_code: '',
-  // time_zone: 'Asia/Seoul',
-  // latitude: 37.5216,
-  // longitude: 126.8583,
-  // metro_code: 0 }
-
-  // { ip: '176.144.16.109',
-  // country_code: 'FR',
-  // country_name: 'France',
-  // region_code: 'HDF',
-  // region_name: 'Hauts-de-France',
-  // city: 'Beauvais',
-  // zip_code: '60000',
-  // time_zone: 'Europe/Paris',
-  // latitude: 49.4333,
-  // longitude: 2.0833,
-  // metro_code: 0 }
-
-  // { ip: '161.109.90.52',
-  // country_code: 'US',
-  // country_name: 'United States',
-  // region_code: 'TX',
-  // region_name: 'Texas',
-  // city: 'Waco',
-  // zip_code: '76705',
-  // time_zone: 'America/Chicago',
-  // latitude: 31.6286,
-  // longitude: -97.0989,
-  // metro_code: 625 }
 
 
   ipget(ip, (err, result) => {
     if (ip != '::1') {
-      locations = db.collection('locations');
+      var locations = db.collection('locations');
       locations.insert(result);
       locations.find().toArray((err1, res) => {
-        console.log(res);
+        res.render('index', { data: res });
+      })
+    } else {
+      var locations = db.collection('locations');
+      locations.find().toArray((err1, result) => {
+      res.render('index', { data: result });
       })
     }
-    
 
-    res.render('index', { title: ip });
   })
 });
 
